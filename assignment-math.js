b79bcb91da4b98f847d0a92dbce46641556e1690
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2025-09-12 21:45:05 -0600

    adding area of circle function to able calculuate Area

---
+++ assignment-math.js
@@ -0,0 +1,6 @@
+// Area of a Circle Functions
+function areaOfCircle(radius) {
+    return Math.PI * Math.pow(radius, 2);
+}
+// Example usage:
+console.log("Area of circle with radius 5:", areaOfCircle(5));
