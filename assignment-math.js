fab20323eac0cf128feb5f3e3e236952c616b0b2
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2025-09-14 18:52:11 -0600

    adding generate password  function to able generate new password

--- assignment-math.js
+++ assignment-math.js
@@ -4,3 +4,22 @@ function areaOfCircle(radius) {
 }
 // Example usage:
 console.log("Area of circle with radius 5:", areaOfCircle(5));
+
+
+// random password generator 
+function generatePassword(length) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+
+    return password;
+}
+
+document.getElementById("passwordBtn").addEventListener("click", () => {
+    const password = generatePassword(10); // 10-character password
+    document.getElementById("passwordOutput").textContent = "Generated Password: " + password;
+});
